b15fdacf430396d5bb71b751861e6e716c2417a2
Amandeep Singh <programmeramandeep@gmail.com>
2024-09-05 14:19:47 +0530

    feat: Lifecycle hooks

--- app/Livewire/Todos.php
+++ app/Livewire/Todos.php
@@ -8,10 +8,29 @@ class Todos extends Component
 {
     public $todo = '';
 
-    public $todos = [
-        'Take out trash',
-        'Do dishes'
-    ];
+    public $todos = [];
+
+    // This method will be called when the component is being mounted
+    public function mount()
+    {
+        $this->todos = [
+            'Learn Laravel',
+            'Learn Vue.js',
+            'Build Awesome Apps',
+        ];
+    }
+
+    // This method will be called when the component is being updated
+    public function updated($property, $value)
+    {
+        $this->$property = strtoupper($value);
+    }
+
+    // This method will be called when the todo property is being updated
+    public function updatedTodo()
+    {
+        $this->todo = strtoupper($this->todo);
+    }
 
     public function add()
     {
