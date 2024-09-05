9343c789de1c09dd881fb53ad2ed6cbf21de8831
Amandeep Singh <programmeramandeep@gmail.com>
2024-09-05 20:30:26 +0530

    chore: Add alpine js

--- resources/views/livewire/create-posts.blade.php
+++ resources/views/livewire/create-posts.blade.php
@@ -10,6 +10,13 @@
         <div>
             <label for="content">Content</label>
             <input type="text" id="content" wire:model='content'>
+            <small>
+                Words:
+                <span x-text="$wire.content.split(' ').length"></span>
+            </small>
+            {{-- <small>Characters:
+                <span x-text="$wire.content.length"></span>
+            </small> --}}
             @error('content')
                 <em>{{ $message }}</em>
             @enderror
